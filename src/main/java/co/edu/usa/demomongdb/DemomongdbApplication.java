f941677e2bd80397d8d37a559aa4882ddc326ad3
julianpizarro <julian.pizarro.mt@correo.usa.edu.co>
2021-11-24 20:25:40 -0500

    Commit inicial

---
+++ src/main/java/co/edu/usa/demomongdb/DemomongdbApplication.java
@@ -0,0 +1,13 @@
+package co.edu.usa.demomongdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomongdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomongdbApplication.class, args);
+	}
+
+}
